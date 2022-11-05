67bd1f09c4689885bc5187f12ea1b5e947302c1e
ahmed-lotfy-dev <elshenawy19@gmail.com>
2022-11-05 20:37:45 +0200

    finishing redux extending tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,31 +1,18 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
-};
-
-const middleWares = [loggerMiddleware];
 
 const persistConfig = {
   key: 'root',
   storage,
   blacklist: ['user'],
 };
+
+const middleWares = [process.evn.NODE_ENV === 'development' && logger].filter(Boolean);
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
